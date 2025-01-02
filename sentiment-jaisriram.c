d34a4b38c3063923f8c73c18f5e115bb125c117f
thewindgod9 <pavan4information@gmail.com>
2025-01-02 21:17:44 +0530

    Update sentiment-jaisriram.c

--- sentiment-jaisriram.c
+++ sentiment-jaisriram.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  printf("Welcome to my first Program of 2025");
+  printf("Enter two nUmber for ADDITION");
+  scanf("%d%d",&a,&b);
+  printf("Addition of Given two numbers is %d",&a+&b);
+}
 
